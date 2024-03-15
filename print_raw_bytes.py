808c081e739fa9694831eb1a7e261fd9920a6852
spfeiffe <60448789+spfeiffe@users.noreply.github.com>
2024-03-15 00:20:06 -0400

    comparing R and Python functions

--- print_raw_bytes.py
+++ print_raw_bytes.py
@@ -2,3 +2,11 @@ x = "0:A" # U+0030, U+003A, U+0041
 for y in x:
   print(hex(ord(y)))
   # print(type(hex(ord(y))))
+
+'''
+R,										Python
+intToUtf8(),							chr()
+utf8ToInt(),							ord()
+str2ucp(),								hex(ord()) # sort of
+bittermelon::ucp2label(str2ucp('A')),	?
+'''
